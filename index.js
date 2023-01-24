a39e7a5d18c0843eca2bd628a09c2b15b1eee89f
Your Name <amos.pun1994@gmail.com>
2023-01-24 13:46:06 +0545

    JavaScript carousel has been appended in front page

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const images = ['cup1.jfif','cup2.jpg','cup3.jpg','cup4.jp',]
+const images = ['cup1.jfif','cup2.jpg','cup3.jpg','cup4.jpg',]
 
-console.log(images)
+// console.log(images)
+
+let index = 3
+document.getElementById('image_slide').src = images[index]
+
+const next = () => {
+    index++
+    if(index == images.length){
+        index = 0
+    }
+    document.getElementById('image_slide').src = images[index]
+}
+
+setInterval(next, 4000)
